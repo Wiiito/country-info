9e42c2183a5f1cfcf8cbdbee40304fac56c3dd58
Wiito <tminemcraft123@gmail.com>
2021-04-29 14:51:46 -0300

    a lot of things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from './components/Layout'
+import GlobalStyle from './components/Theme/globals'
+import { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme } from './components/Theme'
+import React, { useState } from 'react'
+import Home from './components/Home'
 
-function App() {
+const App = () => {
+  const [theme, setTheme] = useState('light')
+  const themeTogler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyle />
+      <Layout togleTheme={themeTogler} theme={theme}>
+        <Home />
+      </Layout>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
